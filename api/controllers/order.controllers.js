568bd6749ffdc82fd64fe64c9894f0d3745a5097
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2023-07-09 21:32:16 +0530

    update

--- api/controllers/order.controllers.js
+++ api/controllers/order.controllers.js
@@ -31,7 +31,7 @@ export const intent = async (req, res, next) => {
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: gig.price * 100,
-    currency: "usd",
+    currency: "inr",
     automatic_payment_methods: {
       enabled: true,
     },
