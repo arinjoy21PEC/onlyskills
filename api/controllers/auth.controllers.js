45330f73129bd48bd440c972ace328d18a5c1b07
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2023-07-21 18:06:16 +0530

    update

--- api/controllers/auth.controllers.js
+++ api/controllers/auth.controllers.js
@@ -37,10 +37,7 @@ export const login = async (req,res,next) =>{
         );
     
         const { password, ...info } = user._doc;
-        res.cookie("accessToken", token, {
-            httpOnly: true,
-            secure: true
-          })
+        res.cookie("accessToken", token)
           .status(200)
           .send(info);
       } catch (err) {
