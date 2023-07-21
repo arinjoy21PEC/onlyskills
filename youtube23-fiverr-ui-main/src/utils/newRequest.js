2d0aa0ed097be0edc4d84d8702c7c64a893df0fd
Arinjoy Nandy <arinjoynandy2019@gmail.com>
2023-07-21 13:45:47 +0530

    updare

--- youtube23-fiverr-ui-main/src/utils/newRequest.js
+++ youtube23-fiverr-ui-main/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://onlyskills-server.onrender.com/api/",
+  baseURL: "https://onlyskills-server.onrender.com/api/",
   withCredentials: true,
 });
 
